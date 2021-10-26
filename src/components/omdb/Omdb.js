86e67cb647e87eb07536d75f77eee3945fabc37e
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-10-26 15:24:05 -0400

    initial save

---
+++ src/components/omdb/Omdb.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Omdb extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Omdb
