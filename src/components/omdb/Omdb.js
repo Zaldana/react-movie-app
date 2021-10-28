1b8fce8d827e76b932b466dfd8795a05845c4f44
Zaldana <77674298+Zaldana@users.noreply.github.com>
2021-10-27 23:44:49 -0400

    ratings not working

--- src/components/omdb/Omdb.js
+++ src/components/omdb/Omdb.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 import axios from "axios";
+import OmdbDetails from './OmdbDetails';
+// import Loading from "../common/Loading";
+// import OmdbDetails from "./OmdbDetails";
 
 export class Omdb extends Component {
 
     state = {
-        title: "matrix",
-        poster: "",
+        moviesArray: [],
         ratingsArray: [],
-        search: "",
-        initialSearch: "",
-        isError: false,
-        errorMessage: "",
+        searchResult: "",
+        // isError: false,
+        // errorMessage: "",
         isLoading: false,
     };
 
+    initialSearch() {
+        
+        const initialSearchArray = ["superman", "lord of the rings", "batman", "pokemon", "harry potter", "star wars", "avengers", "terminator"]
+        
+        const randomMovie = Math.floor(Math.random() * initialSearchArray.length);
+        
+        return initialSearchArray[randomMovie];
+
+    }
+    
     async componentDidMount() {
-        this.fetchMovieApi(this.state.initialSearch);
+        this.fetchMovieApi(this.initialSearch());
     }
 
-    fetchMovieApi = async (search) => {
+    fetchMovieApi = async (searchResult) => {
 
         //setting the state loading is true before api get
         this.setState({
@@ -30,26 +41,24 @@ export class Omdb extends Component {
             const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
             let result = await axios.get(
-                `http://www.omdbapi.com/?s=${search}&apikey=${API_KEY}`
+                `http://www.omdbapi.com/?s=${searchResult}&apikey=${API_KEY}`
             );
             
-            console.log(result);
-            //setting the state with the results of the api 'GET' and error and loading states
-            // this.setState({
-            //     title: result.data.name,
-            //     picture: result.data.sprites.front_default,
-            //     ratingsArray: result.data.abilities,
-            //     isError: false,
-            //     errorMessage: "",
-            //     isLoading: false,
-            // });
+            console.log("result", result);
+         
+            this.setState({
+                moviesArray: result.data.Search,
+                isError: false,
+                errorMessage: "",
+                isLoading: false,
+            });
 
             //Error catch with'e.response' to get full error message response in console
         } catch (e) {
+            
+            // console.log(e.response);
 
-            console.log(e.response);
-
-            //if 'e.response.status = 404 change state to display error
+            // if 'e.response.status = 404 change state to display error
             // if (e && e.response.status === 404) {
             //     this.setState({
             //         isError: true,
@@ -60,17 +69,79 @@ export class Omdb extends Component {
         }
     };
 
+    fetchMovieRating = async (imdbID) => {
+
+        const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+
+        try {
+
+            let result = await axios.get(
+                `http://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`
+            );
+
+            console.log("rating", result.data.imdbRating);
+            // return result.data.imdbRating
+        
+        } catch (e) {
+            
+        }
+    }
+
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    handleOnClick = async () => {
+        this.fetchMovieApi(this.state.searchResult);
+        console.log("state", this.state);
+    };
+
     render() {
         return (
-            <div>
-                <input
-                    name="search"
-                    value={this.state.search}
-                    onChange={this.handleOnChange}
-                />
-                <button onClick={this.handleOnClick}>Search</button>
+            <React.Fragment>
+                <div style={styles.blackBackground}>
+                    <div style={styles.searchDiv}>
+                        <input
+                            name="searchResult"
+                            value={this.state.searchResult}
+                            onChange={this.handleOnChange}
+                            style={styles.search}
+                        />
+                        <button style={styles.button} onClick={this.handleOnClick} >Search</button>
+                    </div>
+                    <OmdbDetails
+                        moviesArray={this.state.moviesArray}
+                    />
+                
             </div>
+            </React.Fragment>
         )
+    }
+}
+
+const styles = {
+
+    blackBackground: {
+        background: "radial-gradient(circle, rgba(32,53,62,1) 0%, rgba(0,0,0,1) 100%)"
+    },
+
+    searchDiv: {
+        background: "linear-gradient(90deg, rgba(10,100,122,1) 0%, rgba(28,38,145,1) 50%, rgba(10,100,122,1) 100%)",
+        padding: 30
+    },
+
+    search: {
+        height: 30,
+        marginRight: "1%",
+        width: "50%",
+    },
+    button: {
+        height: 33,
+        width: 70,
     }
 }
 
--- src/components/omdb/OmdbDetails.js
+++ src/components/omdb/OmdbDetails.js
@@ -1,11 +1,41 @@
-// import PropTypes from "prop-types"
+import PropTypes from "prop-types"
 
 function OmdbDetails(props) {
+
     return (
         <div>
-            
+            <div style={styles.display}>
+                {props.moviesArray.filter((item, index) => index < 8).map(({ Poster, Title, imdbID }) => {
+                    return (
+                        <div key={imdbID} style={styles.row}>
+                            <img src={Poster} alt="poster" />
+                            <h3 key={imdbID}>{Title}</h3>
+                        </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
+const styles = {
+
+    display: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        color: "white"
+    },
+    row: {
+        display: "flex",
+        flexDirection: "column",
+        width: "15%",
+        padding: 50
+    }
+}
+
+OmdbDetails.propTypes = {
+    moviesArray: PropTypes.array.isRequired,
+};
 
 export default OmdbDetails
